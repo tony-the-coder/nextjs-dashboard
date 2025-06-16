13067dc3b87897f0691be3e25f4c7cfa513f8da1
Tony Aldea <tonythecoder@outlook.com>
2025-06-15 20:24:39 -0400

    Refactor dashboard components and remove unused files

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -43,3 +43,30 @@ export default function NavLinks() {
     </>
   );
 }
+// DB Name supabase-orange-xylophone
+
+
+
+// -- Create the table
+// create table notes (
+//   id bigint primary key generated always as identity,
+//   title text not null
+// );
+
+// -- Insert some sample data into the table
+// insert into notes (title)
+// values
+//   ('Today I created a Supabase project.'),
+//   ('I added some data and queried it from Next.js.'),
+//   ('It was awesome!');
+
+// alter table notes enable row level security;
+
+
+
+// create policy "public can read countries"
+// on public.notes
+// for select to anon
+// using (true);
+
+
